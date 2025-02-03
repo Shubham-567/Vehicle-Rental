093356efe6a2caec9d5e5a81a0597e60296de4b7
Shubham <shubhampatil1356@gmail.com>
2025-02-04 00:09:41 +0530

    Added addVehicle, updateVehicle, and deleteVehicle functions to VehicleContext

--- Frontend/src/context/VehicleContext.jsx
+++ Frontend/src/context/VehicleContext.jsx
@@ -34,12 +34,82 @@ export const VehicleProvider = ({ children }) => {
     fetchVehicles();
   }, []);
 
+  // add a new vehicle
+  const addVehicle = useCallback(async (vehicleData) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post(API_URL, vehicleData, {
+        withCredentials: true,
+      });
+      setVehicles((prevVehicles) => [...prevVehicles, response.data.vehicles]);
+    } catch (err) {
+      console.error("Failed to add vehicle", err);
+      setError(
+        err.response?.data?.message ||
+          "Failed to add vehicle. Please try again later. "
+      );
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // update an existing vehicle
+  const updateVehicle = useCallback(async (id, updateData) => {
+    setLoading(true);
+    setError(false);
+
+    try {
+      const response = await axios.put(`${API_URL}/${id}`, updateData, {
+        withCredentials: true,
+      });
+      setVehicles((prevVehicles) =>
+        prevVehicles.map((vehicle) =>
+          vehicle.id === id ? { ...vehicle, ...updatedData } : vehicle
+        )
+      );
+    } catch (err) {
+      console.error("Failed to update vehicle", err);
+      setError(
+        err.response?.data?.message ||
+          "Failed to update vehicle. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  // delete a vehicle
+  const deleteVehicle = useCallback(async (id) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      await axios.delete(`${API_URL}/$id`, { withCredentials: true });
+      setVehicles((prevVehicles) =>
+        prevVehicles.filter((vehicle) => vehicle.id !== id)
+      );
+    } catch (err) {
+      console.error("Failed to delete vehicle", err);
+      setError(
+        err.response?.data?.message ||
+          "Failed to delete vehicle. Please try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
     <VehicleContext.Provider
       value={{
         vehicles,
         loading,
         error,
+        addVehicle,
+        updateVehicle,
+        deleteVehicle,
       }}>
       {children}
     </VehicleContext.Provider>
