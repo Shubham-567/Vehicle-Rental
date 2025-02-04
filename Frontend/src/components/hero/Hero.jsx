1323fd300cb5c73961506a08e8fed474508a0f2f
Shubham <shubhampatil1356@gmail.com>
2025-02-04 17:05:54 +0530

    Added Hero section with styles for home.jsx

--- Frontend/src/components/hero/Hero.jsx
+++ Frontend/src/components/hero/Hero.jsx
@@ -1,10 +1,22 @@
-import "./Hero.css";
+import styles from "./Hero.module.css";
+import heroImg from "../../assets/hero.png";
 
 const Hero = () => {
   return (
-    <section className='hero'>
-      <div>This is hero text</div>
-      <div>This is hero img</div>
+    <section className={styles.hero}>
+      <div className={styles.heroText}>
+        <h1>Find Your Perfect Ride Today</h1>
+        <p>
+          Browse a variety of rental vehicles at great prices. Select your
+          vehicle, location, and dates to get started!
+        </p>
+
+        <div className={styles.searchBox}></div>
+      </div>
+      <div className={styles.heroImage}>
+        <div className={styles.imgBackground}></div>
+        <img src={heroImg} alt='orange jeep' />
+      </div>
     </section>
   );
 };
