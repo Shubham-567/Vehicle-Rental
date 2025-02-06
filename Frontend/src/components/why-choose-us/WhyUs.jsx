46a540d8ba84a08101e3bcfd46c206cd438dd369
Shubham <shubhampatil1356@gmail.com>
2025-02-07 00:43:35 +0530

    created Why Choose Us? section and added styles to it.

---
+++ Frontend/src/components/why-choose-us/WhyUs.jsx
@@ -0,0 +1,89 @@
+import styles from "./WhyUs.module.css";
+import carCollectionIcon from "../../assets/car-collection.png";
+import saveMoneyIcon from "../../assets/save-money.png";
+import paymentIcon from "../../assets/payment.png";
+import supportIcon from "../../assets/support.png";
+
+const WhyUs = () => {
+  return (
+    <section>
+      <div className={styles.sectionHeading}>
+        <h2>Why Choose Us?</h2>
+        <p>
+          Extensive selection, transparent pricing, secure payments, and
+          round-the-clock customer support.
+        </p>
+      </div>
+
+      <div className={styles.benefits}>
+        <div className={styles.benefitCard}>
+          <img
+            src={carCollectionIcon}
+            alt='Illustration of car parking'
+            className={styles.benefitImg}
+          />
+          <div className={styles.benefitText}>
+            <h4>
+              Wide Selection
+              <br /> of Vehicles
+            </h4>
+            <p>
+              From economy cars to luxury rides, we have options for every need.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.benefitCard}>
+          <img
+            src={saveMoneyIcon}
+            alt='Illustration of car parking'
+            className={styles.benefitImg}
+          />
+          <div className={styles.benefitText}>
+            <h4>
+              Affordable & <br /> Transparent Pricing
+            </h4>
+            <p>
+              Clear daily rates with no hidden fees, ensuring a hassle-free
+              rental.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.benefitCard}>
+          <img
+            src={paymentIcon}
+            alt='Illustration of car parking'
+            className={styles.benefitImg}
+          />
+          <div className={styles.benefitText}>
+            <h4>
+              Secure <br />
+              Online Payment
+            </h4>
+            <p>Multiple secure payment options for your peace of mind.</p>
+          </div>
+        </div>
+
+        <div className={styles.benefitCard}>
+          <img
+            src={supportIcon}
+            alt='Illustration of car parking'
+            className={styles.benefitImg}
+          />
+          <div className={styles.benefitText}>
+            <h4>
+              24/7 <br />
+              Customer Support
+            </h4>
+            <p>
+              Our dedicated support team is always ready to assist you, anytime.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyUs;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
 import OurFleet from "../components/our-fleet/OurFeet";
 import Steps from "../components/steps/Steps";
+import WhyUs from "../components/why-choose-us/WhyUs";
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <Steps />
       <hr />
       <OurFleet />
+      <hr />
+      <WhyUs />
       <hr />
     </>
   );
